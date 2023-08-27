4a9eb44319d93c15f83132d4ae7b7ebedbf0d8ee
Dishant Sahu <dishant.sahu07@gmail.com>
2023-08-27 09:58:43 +0530

    ASSIGNMENT

---
+++ index.js
@@ -0,0 +1,44 @@
+
+// Exercises js
+
+console.log("Dishant Sahu "); 
+
+console.log(5 + 10); //15
+console.log(5 -10); // -5
+console.log(5 * 10); // 50
+console.log(5/10); // 0.5
+                         
+let city = "Mumbai";
+console.log(city);
+
+const a = 5;
+var b = 6;
+console.log(a + b );
+
+function sayHello(a){
+    console.log("Hello, [" + a + "]!");
+}
+
+sayHello("Dishant");
+sayHello(295);
+sayHello(true);
+
+function subtract(a,b){
+    return a-b;
+}
+
+console.log(subtract(7,5)); //2
+console.log(subtract(5,7)); //-2
+
+
+const birthYear = 2001;
+
+let num = [1 , 8, 3];
+console.log(num[1]); //8
+
+let book = {
+    title  :"JavaScript",
+     author : "Brendan Eich", 
+     publication_year :1995
+}
+console.log(book.author );
